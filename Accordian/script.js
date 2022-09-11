dddfaeac0edeea1fefd092b84a878989a173b470
David McCutcheon <davidmccutcheon80@gmail.com>
2022-09-11 10:48:38 +0100

    Updated accordian funtionality

--- Accordian/script.js
+++ Accordian/script.js
@@ -3,14 +3,17 @@ const answers = document.querySelectorAll(".answer");
 const chevron = document.querySelectorAll(".chevron");
 
 
-function displaySibling() {
+function displaySibling(e) {
 const selectedAnswer = this.nextElementSibling;
 const selectedQuestion = this;
 const selectedChevron = this.lastElementChild;
 
-console.log(selectedChevron)
-
-answers.forEach(elem => {
+if(e.target === this && e.target.classList.contains("active")) {
+    selectedQuestion.classList.remove(('active'));
+    selectedAnswer.classList.remove(('active'));
+    selectedChevron.classList.remove(('active'));
+} else {
+    answers.forEach(elem => {
     if(elem.classList.contains("active")) {
         elem.classList.remove("active");
     }
@@ -28,9 +31,12 @@ chevron.forEach(elem => {
     }
 })
 
-selectedQuestion.classList.add(('active'));
-selectedAnswer.classList.add(('active'));
-selectedChevron.classList.add(('active'));
+selectedQuestion.classList.toggle(('active'));
+selectedAnswer.classList.toggle(('active'));
+selectedChevron.classList.toggle(('active'));
+}
+
+
 }
 
 
