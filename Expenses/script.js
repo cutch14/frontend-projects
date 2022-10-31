b7cd885acec562961c91b0d8c6860c404c20f200
David McCutcheon <davidmccutcheon80@gmail.com>
2022-10-31 16:12:38 +0000

    Updated functionality

--- Expenses/script.js
+++ Expenses/script.js
@@ -37,19 +37,66 @@ const financeInfo = [
       "day": "sun",
       "amount": 25.48
     }
-  ]
+  ];
+
+  const getCurrentDay = [
+    {
+      0: 6
+    },
+    {
+      1: 0
+    },
+    {
+      2: 1
+    },
+    {
+      3: 2
+    },
+    {
+      4: 3
+    },
+    {
+      5: 4
+    },
+    {
+      6: 5
+    }
+  ];
+
+// Get current day
+
+const date = new Date();
+const day = date.getDay();
+
 
 const dailyAmountColumns = document.querySelectorAll('.column');
 
+// Populate each days spend
+
+function populateDailySpend() {
+  dailyAmountColumns.forEach((el, i) => {
+    el.previousElementSibling.innerHTML = financeInfo[i].amount;
+  })
+};
+
+// Give daily spend columns height
 
 function displayDailyAmountColumsHeights() {
     dailyAmountColumns.forEach((col, i) => {
     setTimeout(() => {
-        col.style.height = `${(150 / 100) * financeInfo[i].amount}px`;
-    console.log(col.clientHeight);
+        col.style.height = `${2.87 * financeInfo[i].amount}px`;
     }, i * 1000)
 });
 };
 
+// Highlight current days column
+
+function highlightCurrentDay() {
+  const today = getCurrentDay[day][day];
+  currentDailyColumn = dailyAmountColumns[today];
+  currentDailyColumn.style.backgroundColor = 'cyan';
+}
 
 document.addEventListener('DOMContentLoaded', displayDailyAmountColumsHeights);
+document.addEventListener('DOMContentLoaded', populateDailySpend);
+document.addEventListener('DOMContentLoaded', highlightCurrentDay);
