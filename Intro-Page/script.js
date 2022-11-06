1ab3c6710d8507dc12fbcb4be449979d3388e13d
David McCutcheon <davidmccutcheon80@gmail.com>
2022-11-06 22:11:38 +0000

    Desktop styling, menu and dropdown functionality

--- Intro-Page/script.js
+++ Intro-Page/script.js
@@ -1,3 +1,5 @@
+// Menu
+
 const menu = document.querySelector(".menu");
 const burger = document.querySelector(".burger");
 const overlay = document.querySelector(".overlay");
@@ -18,3 +20,29 @@ function removeMenuOnScreenClick(e) {
 
 burger.addEventListener('click', toggleMenu);
 overlay.addEventListener('click', removeMenuOnScreenClick);
+
+// Dropdown
+
+const features = document.querySelector(".features");
+const company = document.querySelector(".company");
+const featureDropdown = document.querySelector(".features-options");
+const companyDropdown = document.querySelector(".company-options");
+
+function displayFeatureDropdown() {
+    featureDropdown.classList.toggle("open");
+}
+
+function displayCompanyDropdown() {
+    companyDropdown.classList.toggle("open");
+}
+
+function removeDrowpdown(e) {
+    if(!e.target.classList.contains('features') && !e.target.classList.contains('company')) {
+        featureDropdown.classList.remove('open');
+        companyDropdown.classList.remove('open');
+    }
+}
+
+features.addEventListener('click', displayFeatureDropdown);
+company.addEventListener('click', displayCompanyDropdown);
+document.addEventListener('click', removeDrowpdown);
