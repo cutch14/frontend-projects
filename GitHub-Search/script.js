a2a6bb3d62d1c7af998a7852357f9693bffa3a7e
David McCutcheon <davidmccutcheon80@gmail.com>
2023-01-03 14:39:04 +0000

    Added dark/light mode funcionality

--- GitHub-Search/script.js
+++ GitHub-Search/script.js
@@ -1,4 +1,19 @@
+// Select elements for dark mode switch
 
+const colorSwitchElements = document.querySelectorAll(".color-switch");
+const enableDarkMode = document.querySelector(".light-mode-container");
+const enableLightMode = document.querySelector(".dark-mode-container");
+
+// Toggle dark/light mode
+
+function toggleColorMode() {
+    colorSwitchElements.forEach(elem => elem.classList.toggle("color-switch"));
+}
+
+enableLightMode.addEventListener('click', toggleColorMode);
+enableDarkMode.addEventListener('click', toggleColorMode);
+
+// Get GitHub user data
 
 const endpoint = "https://api.github.com/users/";
 let userName = "octocat";
