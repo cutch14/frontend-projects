1168f243a5878c29344a837cd3022d5291dd638b
David McCutcheon <davidmccutcheon80@gmail.com>
2023-08-13 11:25:06 +0100

    Dropdown select functionality

--- OfficeLite/sign-up.js
+++ OfficeLite/sign-up.js
@@ -1,3 +1,5 @@
+// Countdown 
+
 // grab time remaining elements
 const days = document.querySelector('.days');
 const hours = document.querySelector('.hours');
@@ -34,3 +36,56 @@ function displayTimeRemaining() {
 } 
 
 // setInterval(displayTimeRemaining, 1000);
+
+
+// Menus
+
+const selectPackage = document.querySelector('.subscription-box');
+const packageType = document.querySelector('.pack-type');
+const packagePrice = document.querySelector('.pack-price');
+const chevron = document.querySelector('.chevron');
+const dropdown = document.querySelector('.dropdown');
+const packageOptions = document.querySelectorAll('.dropdown > div');
+const submitBtn = document.querySelector('.list-btn');
+
+function openDropdown() {
+    selectPackage.classList.add('active');
+    dropdown.classList.add('active');
+}
+
+function handleDropdownSelection(e) {
+    packageOptions.forEach(elem => elem.classList.remove('active'));
+    e.currentTarget.classList.add('active');
+    
+    let packText = e.currentTarget.children[0].innerHTML;
+    let packPrice = e.currentTarget.children[1].innerHTML;
+    packageType.innerHTML = packText;
+    packagePrice.innerHTML = packPrice;
+
+    dropdown.classList.remove('active');
+    selectPackage.classList.remove('active');
+}
+
+
+selectPackage.addEventListener('click', openDropdown);
+packageOptions.forEach(elem => {
+    elem.addEventListener('click', handleDropdownSelection);
+})
+
+// Handle input errors
+
+function handleNameInput() {
+
+}
+
+function handleEmailInput() {
+
+}
+
+function handlePhoneInput() {
+
+}
+
+function handleCompanyInput() {
+    
+}
